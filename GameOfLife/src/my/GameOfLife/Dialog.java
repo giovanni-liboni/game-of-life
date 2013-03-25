453f3757a470304d343b406bc932a68e1aa8dfb0
giovanni-liboni <giovanni.liboni92@gmail.com>
2013-03-25 12:11:19 +0100

    Aggiunto dialog

---
+++ GameOfLife/src/my/GameOfLife/Dialog.java
@@ -0,0 +1,181 @@
+package my.GameOfLife;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class Dialog{
+	
+	/*
+	 * Finestra che ritorna il numero di threads inserito
+	 */
+	public void numOfThreads(){
+		String str;
+		int numOfThreads=2;
+		
+		do{
+			str = JOptionPane.showInputDialog (
+					   "Numero di Threads (default 2)", 2);
+				try{
+					int temp = Integer.parseInt(str);
+					if(temp > 0){
+						numOfThreads = temp;
+					}
+					else if(temp < 0)
+						JOptionPane.showMessageDialog(null, "Attenzione valore inserito non valido!\n"+
+								"Inserire numero maggiore di 0.", "Attenzione!", JOptionPane.WARNING_MESSAGE);
+					
+				}
+				catch(NumberFormatException e){
+			}
+			}
+			while(numOfThreads < 0);
+		// modifico il numero di threads
+		Finestra.numOfThreads = numOfThreads;
+		Finestra.frame.setNumOfThreadLabel(String.valueOf(numOfThreads));
+
+	}
+	public void fileSave(Core campo,int contGen, int numOfThreads ){
+		
+		String
+			fileName = "";
+		
+		JFileChooser c = new JFileChooser();
+		
+		int rVal = c.showSaveDialog(null);
+		if(rVal == JFileChooser.APPROVE_OPTION) {
+			fileName = c.getSelectedFile().getName();
+		}
+		if(rVal == JFileChooser.CANCEL_OPTION) {
+			fileName = "";
+		}
+		if(fileName == null){
+//			dispose();
+		}
+		File f = c.getSelectedFile();
+		
+		try {
+		
+			DataOutputStream outStream = new DataOutputStream(new FileOutputStream(f));
+			
+			int max = campo.getArray().length;
+			
+			outStream.writeInt(max);
+			outStream.writeChar(':');
+			
+			outStream.writeInt(contGen);
+			outStream.writeChar(':');
+			
+			outStream.writeInt(numOfThreads);
+			outStream.writeChar(':');
+			
+			for(int y=0; y< max;y++){
+				for(int x=0; x < max;x++){
+					
+					outStream.writeBoolean(campo.getArray()[y][x]);
+					outStream.writeChar(';');
+				}
+			}
+			
+			outStream.close();
+			
+		} catch (FileNotFoundException e) {
+			JOptionPane.showConfirmDialog(null, "File non trovato", "Attenzione!", JOptionPane.ERROR_MESSAGE);
+		} catch (IOException e) {
+			JOptionPane.showConfirmDialog(null, "Salvataggio non riuscito", "Attenzione!", JOptionPane.ERROR_MESSAGE);
+			
+		}
+	}
+	public void fileOpen(){
+
+		String
+			fileName = "";
+		
+		JFileChooser c = new JFileChooser();
+		
+		int rVal = c.showOpenDialog(null);
+		if(rVal == JFileChooser.APPROVE_OPTION) {
+			fileName = c.getSelectedFile().getName();
+		
+		}
+		if(rVal == JFileChooser.CANCEL_OPTION) {
+			fileName = "";
+			
+		}
+		if(fileName == null){
+			JOptionPane.showConfirmDialog(null, "File non trovato", "Attenzione!", JOptionPane.ERROR_MESSAGE);
+			
+		}
+		File f = c.getSelectedFile();
+		DataInputStream inStream;
+		try {
+				
+			inStream = new DataInputStream(new FileInputStream(f));
+	
+			Core campo;
+			boolean [][] array = null;
+			int max;
+			try{
+				
+				max = inStream.readInt();
+				inStream.readChar();
+				
+				// contGen
+				Finestra.contGen = inStream.readInt();
+				inStream.readChar();
+				
+				//numOfThreads
+				Finestra.numOfThreads = inStream.readInt();
+				inStream.readChar();
+				
+				array = new boolean[max][max];
+				
+				for(int y=0; y< max;y++){
+					for(int x=0; x < max;x++){
+						array[y][x] = inStream.readBoolean();
+						inStream.readChar();
+					}
+				}
+				
+				campo = new Core(Finestra.numOfThreads, array);
+				Finestra.cont.remove(Finestra.panel);
+				Finestra.panel = new Griglia(campo);
+				Finestra.cont.add(Finestra.panel);
+				Finestra.panel.setVisible(false);
+				Finestra.panel.setVisible(true);          
+				Finestra.gameStatus = false;
+				
+			    Finestra.frame.setContGenLabel(String.valueOf(Finestra.contGen));
+				Finestra.frame.setNumOfThreadLabel(String.valueOf(Finestra.numOfThreads));
+				
+				
+			}
+			catch(EOFException e){
+				JOptionPane.showConfirmDialog(null, "Caricamento completato");
+				
+			}
+			inStream.close();
+			
+		} 
+		catch (FileNotFoundException e) {
+			JOptionPane.showConfirmDialog(null, "File non trovato", "Attenzione!", JOptionPane.ERROR_MESSAGE);
+		} 
+		catch (IOException e) {
+			JOptionPane.showConfirmDialog(null, "Caricamento non riuscito", "Attenzione!", JOptionPane.DEFAULT_OPTION);
+			
+		}
+		catch (Exception e) {
+		JOptionPane.showConfirmDialog(null, "Caricamento fallito", "Attenzione!", JOptionPane.DEFAULT_OPTION);
+		}
+	}
+
+}
--- GameOfLife/src/my/GameOfLife/Finestra.java
+++ GameOfLife/src/my/GameOfLife/Finestra.java
@@ -2,28 +2,22 @@ package my.GameOfLife;
 
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
-
 import java.io.*;
 
 @SuppressWarnings("serial")
 public class Finestra extends JFrame{
 	
-	private String
-		fileName = "",
-		str;
-
-	private boolean gameStatus; // if true -> start else pause
-	private GameX frame;
-	private Griglia panel;
-	private ColorChanger colorChanger;
-	private int 
+	private Dialog dialog;
+	protected static boolean gameStatus; // if true -> start else pause
+	protected static GameX frame;
+	protected static Griglia panel;
+	protected static int 
 		numOfThreads=1,
-		X=50, 
-		Y=50,
+		X=80, 
+		Y=80,
 		contGen=0;
-	private Container cont;
+	protected static Container cont;
 	final Runnable doNextGen = new Runnable() {
 	     public void run() {
 	         nextGen();
@@ -34,32 +28,13 @@ public class Finestra extends JFrame{
 	 * and then creates game main window
 	 */
 	public Finestra(){
+		
+		dialog = new Dialog();
+		
 		/* Creo la finestra iniziale e aggiorno il numero di threads
 		 * da usare
 		 */
-		do{
-			JOptionPane paneThreads = new JOptionPane();
-			JDialog diagThreads = paneThreads.createDialog("Let's start");
-			str = paneThreads.showInputDialog ("Numero di Threads (default 2)", 2);
-//			JOptionPane.getRootFrame().dispose();
-			try{
-				if (str == null) {
-				        System.exit(0);
-				}
-				int temp = Integer.parseInt(str);
-				if(temp > 0){
-					numOfThreads = temp;
-				}
-				else if(temp < 0)
-					JOptionPane.showMessageDialog(null, "Attenzione valore inserito non valido!\n"+
-					"Inserire numero maggiore di 0.", "Attenzione!", JOptionPane.WARNING_MESSAGE);
-				}
-			catch(NumberFormatException e){
-				numOfThreads = -1;
-				JOptionPane.showMessageDialog(null, "Attenzione valore inserito non valido!\n" +
-				"Inserire un intero maggiore di 0.", "Attenzione!", JOptionPane.WARNING_MESSAGE);
-			}
-		}while(numOfThreads < 0);
+		dialog.numOfThreads();
 		
 		/* Creo il frame*/
 		
@@ -98,79 +73,8 @@ public class Finestra extends JFrame{
 					new ActionListener(){
 						@Override
 						public void actionPerformed(ActionEvent arg0) {
-							JFileChooser c = new JFileChooser();
-							
-							int rVal = c.showOpenDialog(Finestra.this);
-							if(rVal == JFileChooser.APPROVE_OPTION) {
-								fileName = c.getSelectedFile().getName();
-							
-							}
-							if(rVal == JFileChooser.CANCEL_OPTION) {
-								fileName = "";
-								
-							}
-							if(fileName == null){
-								dispose();
-								JOptionPane.showConfirmDialog(getParent(), "File non trovato", "Attenzione!", ERROR, ERROR);
-								
-							}
-							File f = c.getSelectedFile();
-							DataInputStream inStream;
-							try {
-								
-								inStream = new DataInputStream(new FileInputStream(f));
-			
-							Core campo;
-							boolean [][] array = null;
-							int max;
-							try{
-								
-								max = inStream.readInt();
-								inStream.readChar();
-		
-								contGen = inStream.readInt();
-								inStream.readChar();
-								
-								numOfThreads = inStream.readInt();
-								inStream.readChar();
-								
-								array = new boolean[max][max];
-								
-								for(int y=0; y< max;y++){
-									for(int x=0; x < max;x++){
-										array[y][x] = inStream.readBoolean();
-										inStream.readChar();
-									}
-								}
-								
-								campo = new Core(numOfThreads, array);
-								cont.remove(panel);
-								panel = new Griglia(campo);
-								cont.add(panel);
-								panel.setVisible(false);
-								panel.setVisible(true);
-								
-							    frame.setContGenLabel(String.valueOf(contGen));
-								frame.setNumOfThreadLabel(String.valueOf(numOfThreads));
-			                    
-								gameStatus = false;
-								disegna();
-							}
-							catch(EOFException e){
-								JOptionPane.showConfirmDialog(getParent(), "Caricamento completato");
-								
-							}
-		
-							inStream.close();
-							} catch (FileNotFoundException e) {
-								JOptionPane.showConfirmDialog(getParent(), "File non trovato", "Attenzione!", ERROR);
-							} catch (IOException e) {
-								JOptionPane.showConfirmDialog(getParent(), "Caricamento non riuscito", "Attenzione!", JOptionPane.DEFAULT_OPTION);
-								
-							}
-								catch (Exception e) {
-							JOptionPane.showConfirmDialog(null, "Caricamento fallito", "Attenzione!", JOptionPane.DEFAULT_OPTION);
-							}
+							dialog.fileOpen();		
+							disegna();
 					}
 				},
 			
@@ -178,51 +82,7 @@ public class Finestra extends JFrame{
 				new ActionListener(){
 					@Override
 					public void actionPerformed(ActionEvent arg0) {
-						JFileChooser c = new JFileChooser();
-						
-						int rVal = c.showSaveDialog(Finestra.this);
-						if(rVal == JFileChooser.APPROVE_OPTION) {
-							fileName = c.getSelectedFile().getName();
-						}
-						if(rVal == JFileChooser.CANCEL_OPTION) {
-							fileName = "";
-						}
-						if(fileName == null){
-							dispose();
-						}
-						File f = c.getSelectedFile();
-						
-						try {
-						
-							DataOutputStream outStream = new DataOutputStream(new FileOutputStream(f));
-							Core campo = panel.getCampo();
-							int max = campo.getArray().length;
-							
-							outStream.writeInt(max);
-							outStream.writeChar(':');
-							
-							outStream.writeInt(contGen);
-							outStream.writeChar(':');
-							
-							outStream.writeInt(numOfThreads);
-							outStream.writeChar(':');
-							
-							for(int y=0; y< max;y++){
-								for(int x=0; x < max;x++){
-									
-									outStream.writeBoolean(campo.getArray()[y][x]);
-									outStream.writeChar(';');
-								}
-							}
-							
-							outStream.close();
-							
-						} catch (FileNotFoundException e) {
-							JOptionPane.showConfirmDialog(getParent(), "File non trovato", "Attenzione!", ERROR, ERROR);
-						} catch (IOException e) {
-							JOptionPane.showConfirmDialog(getParent(), "Salvataggio non riuscito", "Attenzione!", ERROR, ERROR);
-							
-						}
+						dialog.fileSave(panel.getCampo(), contGen, numOfThreads);
 					}
 				},
 				
@@ -256,31 +116,9 @@ public class Finestra extends JFrame{
 				new ActionListener(){
 					@Override
 					public void actionPerformed(ActionEvent arg0) {
-					
-						do{
-							str = JOptionPane.showInputDialog (
-									   "Numero di Threads (default 2)", 2);
-								try{
-									int temp = Integer.parseInt(str);
-									if(temp > 0){
-										numOfThreads = temp;
-									}
-									else
-										JOptionPane.showMessageDialog(null, "Attenzione valore inserito non valido!\n"+
-												"Inserire numero maggiore di 0.", "Attenzione!", JOptionPane.WARNING_MESSAGE);
-									
-								}
-								catch(Exception e){
-									numOfThreads = -1;
-									JOptionPane.showMessageDialog(null, "Attenzione valore inserito non valido!\n" +
-											"Inserire un intero maggiore di 0.", "Attenzione!", JOptionPane.WARNING_MESSAGE);
-								}
-							}
-							while(numOfThreads < 0);
-						// aggiornamento numero di threads usate stampate
-						frame.setNumOfThreadLabel(str);
+						dialog.numOfThreads();
 					}
-					},
+				},
 					// chiude il programma	- 4
 						
 				new ActionListener(){
